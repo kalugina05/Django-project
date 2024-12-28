351c182b17ff045028306e86fa10f3e575d8dd33
Анна Калугина <a.a,kalugina@urfu.me>
2024-12-28 23:01:50 +0500

    Скелет проекта

--- djangoProject/main/urls.py
+++ djangoProject/main/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('general_statistics', views.general_statistics),
+    path('geography', views.geography),
+    path('relevance', views.relevance),
+    path('skills', views.skills),
+    path('recent_jobs', views.recent_jobs),
 ]
--- djangoProject/main/views.py
+++ djangoProject/main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
@@ -7,3 +6,15 @@ def index(request):
 
 def general_statistics(request):
     return render(request, 'main/statistics.html')
+
+def geography(request):
+    return render(request, 'main/geography.html')
+
+def relevance(request):
+    return render(request, 'main/relevance.html')
+
+def skills(request):
+    return render(request, 'main/skills.html')
+
+def recent_jobs(request):
+    return render(request, 'main/recent_jobs.html')
