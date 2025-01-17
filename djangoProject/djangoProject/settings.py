051b93315fd9eeed56b49e34629111a77049e0fd
Анна Калугина <a.a,kalugina@urfu.me>
2025-01-17 20:38:52 +0500

    statichost

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -116,14 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'staticgr/'
+STATIC_URL = '/staticgr/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "staticgr",
     '/var/www/static',
 ]
 
-STATIC_ROOT = 'home/kalugina05/Django-project/staticgr'
+STATIC_ROOT = 'home/kalugina05/Django-project/djangoProject/staticgr'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
